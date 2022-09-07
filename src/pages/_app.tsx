0640f0d89c3c55f65b79f4791567b636bf680324
Amadeusz Grzesiak <adek-g@go2.pl>
2022-09-07 11:53:44 +0200

    added GA4 - google analitics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/inline-script-id */
 /* eslint-disable @next/next/no-page-custom-font */
 import React, { useEffect, useState } from 'react';
 import { AppProps } from 'next/app';
@@ -6,9 +7,23 @@ import '../components/Skills/Skills.scss';
 import Menu from '../components/Menu/Menu';
 import Footer from '../components/Footer/Footer';
 import Cookies from '../components/Cookies/Cookies';
+import Script from 'next/script';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [showCookies, setShowCookies] = useState(false);
+
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on(`routeChangeComplete`, handleRouteChange);
+    return () => {
+      router.events.off(`routeChangeComplete`, handleRouteChange);
+    };
+  }, [router.events]);
 
   useEffect(() => {
     setShowCookies(
@@ -23,6 +38,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-JDJ12Z6J8M`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JDJ12Z6J8M', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Menu />
       <Component {...pageProps} />
       <Footer />
